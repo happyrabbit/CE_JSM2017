7151e4111ede9fc07da2260cfcbe0acc54b63a1b
Hui Lin <longqiman@gmail.com>
2017-07-09 23:06:36 -0500

    reproducible report slides

--- Rcode/00-course-setup.R
+++ Rcode/00-course-setup.R
@@ -10,7 +10,9 @@
 # install packages from CRAN
 p_needed <- c("plyr", "dplyr", "caret","e1071","gridExtra","lattice","imputeMissings",
               "RANN","corrplot","nnet","car","gpairs","reshape2","psych","tidyr",
-              "ggplot2")
+              "ggplot2","sparklyr","readr","mvtnorm","MASS","data.table","magrittr",
+              "shiny","metricsgraphics","DT","leaflet","dygraphs","xts","lubridate",
+              "highcharter","rbokeh","networkD3","threejs")
 packages <- rownames(installed.packages())
 p_to_install <- p_needed[!(p_needed %in% packages)]
 if (length(p_to_install) > 0) {
